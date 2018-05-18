8e28cb6d7478c32a7bac6529a574a09866095059
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-17 20:11:04 -0500

    Update cmds.js

--- commands/cmds.js
+++ commands/cmds.js
@@ -5,7 +5,7 @@ let help = new Discord.RichEmbed()
 .setAuthor(bot.user.username, bot.user.avatarURL)
 .setColor("#3afff8")
 .setDescription("~Here Are All The Commands~")
-.addField("!ping", Used To Check The Response Time On The Bot");
+.addField("!ping", "Used To Check The Response Time On The Bot");
 
 message.channel.send(help);
 }
