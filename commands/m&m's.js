c6c35d7ed5542fda6082827797b331970ab9eaa5
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-17 20:28:25 -0500

    Create m&m's.js

---
+++ commands/m&m's.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+let bal = require("../m&m's.json");
+
+module.exports.run = async (bot, message, args) => {
+  if(!bal[message.author.id]){
+    bal[message.author.id] = {
+      coins: 0
+    };
+  }
+
+  let uCoins = bal[message.author.id].bal;
+
+
+  let coinEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#00FF00")
+  .addField("You Have", `${uCoins} M&M's!`);
+
+  message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+
+}
+
+module.exports.help = {
+  name: "bal"
+}
