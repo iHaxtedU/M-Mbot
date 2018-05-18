518c60381a7e3f09adb0ed1bfa9ca43bee5be5a0
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-17 20:37:15 -0500

    Update pay.js

--- commands/pay.js
+++ commands/pay.js
@@ -5,13 +5,13 @@ let bal = require("../m&m's.json");
 module.exports.run = async (bot, message, args) => {
 
   if(!bal[message.author.id]){
-    return message.reply("You don't have any coins!")
+    return message.reply("You don't have any M&M's!")
   }
 
   let pUser = message.mentions.users.first();
 
-  if(!coins[pUser.id]){
-    coins[pUser.id] = {
+  if(!bal[pUser.id]){
+    bal[pUser.id] = {
       coins: 150
     };
   }
@@ -19,7 +19,7 @@ module.exports.run = async (bot, message, args) => {
   let pCoins = bal[pUser.id].bal;
   let sCoins = bal[message.author.id].bal;
 
-  if(sCoins < args[0]) return message.reply("Not enough coins there!");
+  if(sCoins < args[0]) return message.reply("Not enough M&M's there!");
 
   bal[message.author.id] = {
     bal: sCoins - parseInt(args[1])
@@ -29,7 +29,7 @@ module.exports.run = async (bot, message, args) => {
      bal: pCoins + parseInt(args[1])
   };
 
-  message.channel.send(`${message.author} has given ${pUser} ${args[1]} coins.`);
+  message.channel.send(`${message.author} has given ${pUser} ${args[1]} M&M's.`);
 
   fs.writeFile("./m&m's.json", JSON.stringify(coins), (err) => {
     if(err) cosole.log(err)
