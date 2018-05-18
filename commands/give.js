4860188c7ca4faf2656805c77c41ab4fc5f9e00c
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-17 23:03:32 -0500

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -4,7 +4,7 @@ let bal = require("../m&m's.json");
 
 module.exports.run = async (bot, message, args) => {
   
-  if (message.author.hasPermission("ADMINISTRATOR")) return message.channel.send("You Need Permission To Do This");
+  if (message.author.hasPermissions("ADMINISTRATOR")) return message.channel.send("You Need Permission To Do This");
 
   let pUser = message.mentions.users.first();
   if (!pUser) return message.channel.send("Please Specify A User")
