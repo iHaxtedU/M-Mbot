ca36bb6601885f49b72f7cd5735dc2fe57125a1d
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-17 23:08:28 -0500

    Create take.js

---
+++ commands/take.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+let bal = require("../m&m's.json");
+
+module.exports.run = async (bot, message, args) => {
+  
+if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You Do Not Have This Permission");
+
+  let pUser = message.mentions.users.first();
+  if (!pUser) return message.channel.send("Please Specify A User")
+
+  if(!bal[pUser.id]){
+    bal[pUser.id] = {
+      coins: 150
+    };
+  }
+
+  let pCoins = bal[pUser.id].bal;
+
+  if(isNaN(parseInt(args[1]))) return message.channel.send("Use A Number!");
+  if (message.content.includes("-")) return message.channel.send("Don't Use A Negative");
+  if (message.content.includes(".")) return message.channel.send("No Periods!")
+
+  bal[pUser.id] = {
+     bal: pCoins - parseInt(args[1])
+  };
+
+  message.channel.send(`${message.author} has given ${pUser} ${args[1]} M&M's.`);
+
+  fs.writeFile("./m&m's.json", JSON.stringify(bal), (err) => {
+    if(err) cosole.log(err)
+  });
+
+
+}
+
+module.exports.help = {
+  name: "give"
+}
