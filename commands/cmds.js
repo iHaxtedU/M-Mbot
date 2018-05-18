f0c83b7d701b8662f1f84ea23d1bbc4885091b14
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-17 20:10:15 -0500

    Create cmds.js

---
+++ commands/cmds.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+let help = new Discord.RichEmbed()
+.setAuthor(bot.user.username, bot.user.avatarURL)
+.setColor("#3afff8")
+.setDescription("~Here Are All The Commands~")
+.addField("!ping", Used To Check The Response Time On The Bot");
+
+message.channel.send(help);
+}
+
+module.exports.help = {
+  name: "cmds"
+}
