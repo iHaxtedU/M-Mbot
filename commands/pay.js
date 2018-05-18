7d2d467699438c1959f3ae684f5a044660df074e
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-17 20:46:52 -0500

    Update pay.js

--- commands/pay.js
+++ commands/pay.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
   }
 
   let pUser = message.mentions.users.first();
-  if (pUser = message.author) return message.channel.send("You Can't Pay Yourself")
+  if (pUser.id = message.author) return message.channel.send("You Can't Pay Yourself");
 
   if(!bal[pUser.id]){
     bal[pUser.id] = {
