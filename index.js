9bf45625bf5312852712e33689ccccfd7b406d17
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-17 19:41:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,12 +24,14 @@ fs.readdir("./commands/", (err, files) => {
 
 
 bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-let startEmbed = new Discord.RichEmbed()
+console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+ 
+  let startEmbed = new Discord.RichEmbed()
 .setAuthor(bot.user.username, bot.user.avatarURL)
 .setDescription("Bot Successfully Restarted");
   
 bot.channels.find('name', "chat").send(startEmbed);
+  
 bot.user.setActivity("Getting Updated", {type: "PLAYING"});
 });
 
