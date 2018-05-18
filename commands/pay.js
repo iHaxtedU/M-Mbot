6489a9160ceacaabd884c37ca69a9a1df23088d2
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-17 21:15:58 -0500

    Update pay.js

--- commands/pay.js
+++ commands/pay.js
@@ -22,7 +22,7 @@ module.exports.run = async (bot, message, args) => {
   let sCoins = bal[message.author.id].bal;
 
   if(isNaN(parseInt(args[1]))) return message.channel.send("Use A Number!");
-  if (message.content.startsWith(`!pay ${pUser} -`)) return message.channel.send("Give Them Negatives And I Give You Ban...");
+  if (message.content.startsWith(`!pay ${pUser} -${parseInt(args[1])}`)) return message.channel.send("Give Them Negatives And I Give You Ban...");
 
   if(sCoins < parseInt(args[1])) return message.reply("Not enough M&M's there!");
 
