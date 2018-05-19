8b190ee93e1d3d4f6dc634a45846a43242550edd
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-19 01:39:54 -0500

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -23,9 +23,11 @@ module.exports.run = async (bot, message, args) => {
 
   let kickChannel = message.guild.channels.find(`name`, "bot-logs");
   if (!kickChannel) return message.channel.send("Error Finding Channel, Please DM @Ez Potato#2648");
+  
+  let send1 = "`kReason`"
 
 try {
- kUser.send(`You Were Kicked From MMMHub For $`{kReason}` Don't Do It Again...`);
+ kUser.send(`You Were Kicked From MMMHub For ${send1} Don't Do It Again...`);
  kickChannel.send(kickEmbed);
  message.channel.send(msg);
  kUser.kick();
